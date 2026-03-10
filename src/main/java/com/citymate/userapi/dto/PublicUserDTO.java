dbfa44cfa284fd1ad27baca9817b802b50d61fc2
Brahimdiarra <brahim.abdidiarra@etudiant.univ-brest.fr>
2026-03-10 06:03:22 +0100

    suppression de jwt provider et rajouter des endpointds pour la gestion des admins

---
+++ src/main/java/com/citymate/userapi/dto/PublicUserDTO.java
@@ -0,0 +1,35 @@
+package com.citymate.userapi.dto;
+
+import com.citymate.userapi.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * DTO public pour afficher le profil d'un autre utilisateur.
+ * Ne contient que les informations visibles publiquement.
+ * N'expose PAS : email, id, isActive, isVerified, createdAt
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PublicUserDTO {
+
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String profilePictureUrl;
+    private String bio;
+    private String city;
+    private User.ProfileType profileType;
+
+    public PublicUserDTO(User user) {
+        this.username = user.getUsername();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.profilePictureUrl = user.getProfilePictureUrl();
+        this.bio = user.getBio();
+        this.city = user.getCity();
+        this.profileType = user.getProfileType();
+    }
+}
